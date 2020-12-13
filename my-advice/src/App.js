6e0236634b6107fa958c9ae45649b9b7740d6ed7
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-12-13 18:29:27 +0200

    corrections

--- my-advice/src/App.js
+++ my-advice/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import axios from 'axios';
 
 import './App.css';
@@ -39,3 +39,4 @@ class App extends Component {
 }
 
 export default App;
+
